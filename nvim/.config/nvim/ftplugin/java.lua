6f5f3c92738845e4abbaf5962c0ce9f9cc31565d
Chainso <chanzobryan@gmail.com>
2022-11-12 00:24:59 -0800

    nvim: Added semantic tokens

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -21,6 +21,21 @@ require("jdtls").start_or_attach({
     "-data", root_dir .. "/" .. data_dir_name
   },
   root_dir = root_dir,
-  capabilities = capabilities
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    local caps = client.server_capabilities
+    if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+      local augroup = vim.api.nvim_create_augroup("SemanticTokens", {})
+      vim.api.nvim_create_autocmd("TextChanged", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.semantic_tokens_full()
+        end,
+      })
+      -- fire it first time on load as well
+      vim.lsp.buf.semantic_tokens_full()
+    end
+  end
 })
 
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -213,12 +213,42 @@ local packer_startup = require("packer").startup(function(use)
       mason_lspconfig.setup_handlers({
         function (server_name)
           lspconfig[server_name].setup({
-            capabilities = capabilities
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              local caps = client.server_capabilities
+              if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+                local augroup = vim.api.nvim_create_augroup("SemanticTokens", {})
+                vim.api.nvim_create_autocmd("TextChanged", {
+                  group = augroup,
+                  buffer = bufnr,
+                  callback = function()
+                    vim.lsp.buf.semantic_tokens_full()
+                  end,
+                })
+                -- fire it first time on load as well
+                vim.lsp.buf.semantic_tokens_full()
+              end
+            end
           })
         end,
         ["sumneko_lua"] = function()
           require"lspconfig".sumneko_lua.setup {
             capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              local caps = client.server_capabilities
+              if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+                local augroup = vim.api.nvim_create_augroup("SemanticTokens", {})
+                vim.api.nvim_create_autocmd("TextChanged", {
+                  group = augroup,
+                  buffer = bufnr,
+                  callback = function()
+                    vim.lsp.buf.semantic_tokens_full()
+                  end,
+                })
+                -- fire it first time on load as well
+                vim.lsp.buf.semantic_tokens_full()
+              end
+            end,
             settings = {
               Lua = {
                 runtime = {
@@ -246,7 +276,6 @@ local packer_startup = require("packer").startup(function(use)
     end
   }
 
-  -- Completion
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-buffer"
   use "hrsh7th/cmp-path"
@@ -256,6 +285,21 @@ local packer_startup = require("packer").startup(function(use)
 
   use "L3MON4D3/LuaSnip"
   use "saadparwaiz1/cmp_luasnip"
+
+  use {
+    "theHamsta/nvim-semantic-tokens",
+    config = function()
+      require("nvim-semantic-tokens").setup {
+        preset = "default",
+        -- highlighters is a list of modules following the interface of nvim-semantic-tokens.table-highlighter or 
+        -- function with the signature: highlight_token(ctx, token, highlight) where 
+        --        ctx (as defined in :h lsp-handler)
+        --        token  (as defined in :h vim.lsp.semantic_tokens.on_full())
+        --        highlight (a helper function that you can call (also multiple times) with the determined highlight group(s) as the only parameter)
+        highlighters = { require "nvim-semantic-tokens.table-highlighter"}
+      }
+    end
+  }
 
   -- Vim practice
   use "ThePrimeagen/vim-be-good"
