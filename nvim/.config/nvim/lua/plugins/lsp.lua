ddc1dcf6ba415f6d1f71228455980ce3658054ea
Chainso <chanzobryan@gmail.com>
2023-03-19 16:34:12 -0700

    nvim: added keybindings for rust-tools

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -198,11 +198,17 @@ local setup = function(use)
           end
         end,
         ["rust_analyzer"] = function()
-          require("rust-tools").setup({
+          local rt = require("rust-tools")
+
+          rt.setup({
             server = {
               on_attach = function(_, bufnr)
+                -- Hover actions
+                vim.keymap.set("n", "<C-Space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                -- Code action groups
+                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
               end
-            },
+            }
           })
         end
       })
