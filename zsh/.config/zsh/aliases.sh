7ead3faa5c1c232b89d3d44426e53159d3fd190e
Chainso <chanzobryan@gmail.com>
2023-03-29 20:48:02 -0700

    zsh: Added more convenient git and apt aliases

--- zsh/.config/zsh/aliases.sh
+++ zsh/.config/zsh/aliases.sh
@@ -1,9 +1,12 @@
 # All custom aliases
 
 # Apt
-alias aptud="sudo apt update"
-alias aptug="sudo apt upgrade"
-alias aptudg="sudo apt update && sudo apt upgrade"
+alias aptupd="sudo apt update"
+alias aptupg="sudo apt upgrade"
+alias aptupdg="sudo apt update && sudo apt upgrade"
+alias aptrm="sudo apt remove"
+alias aptin="sudo apt install"
+alias aptar="sudo apt autoremove"
 
 # Pacman
 alias pacview="pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
@@ -11,4 +14,15 @@ alias pacup="sudo pacman -Syu"
 
 # Paru
 alias paruin="paru -Slq | fzf -m --preview 'cat <(paru -Si {1}) <(paru -Fl {1} | awk \"{print $2}\")' | xargs -ro paru -S"
+
+# Git
+function gsq() {
+  git reset --soft HEAD~$1
+  git commit --amend --no-edit
+}
+
+function gcsq() {
+  git commit -m "Throwaway message"
+  gsq $1
+}
 
