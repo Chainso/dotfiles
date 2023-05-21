c934e6267f0f672d8baab7b32ec81662dd6592be
Chainso <chanzobryan@gmail.com>
2023-05-21 12:16:50 -0700

    nvim: Adding lsp_signature

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -101,6 +101,18 @@ local setup = function(use)
   }
 
   use {
+    "ray-x/lsp_signature.nvim",
+    config = function()
+      require("lsp_signature").setup({
+        bind = true, -- This is mandatory, otherwise border config won't get registered.
+        handler_opts = {
+          border = "rounded"
+        }
+      })
+    end
+  }
+
+  use {
     "smjonas/inc-rename.nvim",
     config = function()
       require("inc_rename").setup()
