a5e89e7e424d58874a57cacbac54cec8f71e1e96
Chainso <chanzobryan@gmail.com>
2023-10-03 22:07:00 -0700

    fzf: Using fd to search

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,6 +1,11 @@
 # Setup fzf
 # ---------
 
+# Find Command
+# ---------
+export FZF_DEFAULT_COMMAND='fd -H --strip-cwd-prefix'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "$/usr/share/fzf/completion.zsh" 2> /dev/null
