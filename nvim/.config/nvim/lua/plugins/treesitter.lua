58b3e5960c282407550469a601660fdc37fcb520
Chainso <chanzobryan@gmail.com>
2023-11-14 21:24:43 -0800

    nvim: Better venv support for python and treesitter highlight/increment selection by default

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,27 @@
 return {
   {
-    "hiphish/rainbow-delimiters.nvim",
-    build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        highlight = {
+          enable = true
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "gni",
+            scope_incremental = "gns",
+            node_decremental = "gnd",
+          },
+        }
+      }
+    end
   }
+  -- {
+  --   "hiphish/rainbow-delimiters.nvim",
+  --   build = ":TSUpdate"
+  -- }
 }
 
