619a5b1de86ebc835272f17edf3d20ae9005aaf6
Chanzo Bryan <chanzob@netflix.com>
2024-09-12 10:29:33 -0700

    nvim: added which-key to see commands while typing

--- nvim/.config/nvim/lua/plugins/command.lua
+++ nvim/.config/nvim/lua/plugins/command.lua
@@ -9,6 +9,25 @@ return {
     end
   },
 
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+
   -- Git integration
   "tpope/vim-fugitive"
 }
