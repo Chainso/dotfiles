9c364622fa8bb914d67da27fa3df236f2649f392
Chanzo Bryan <chanzob@netflix.com>
2024-09-12 10:25:15 -0700

    nvim: added todo-comments

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -137,6 +137,16 @@ return {
     end
   },
 
-  "chrisbra/csv.vim"
+  "chrisbra/csv.vim",
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 }
 
