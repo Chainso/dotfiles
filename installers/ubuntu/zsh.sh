4cdf2a7bd81d0646f2d8467958239f7084e2d4ee
Chanzo Bryan <chanzob@netflix.com>
2024-10-15 12:17:27 -0700

    Removed oh-my-zsh from install since it is prepackaged in config

--- installers/ubuntu/zsh.sh
+++ installers/ubuntu/zsh.sh
@@ -2,6 +2,3 @@
 
 sudo apt install -y zsh
 sudo chsh -s $(which zsh) $(whoami)
-
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
