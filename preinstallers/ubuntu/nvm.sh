df3553ea187ee8086b3fe659206f456aa7aff3c3
Chanzo Bryan <chanzob@netflix.com>
2024-10-16 10:54:44 -0700

    fixed nvm preinstall

--- preinstallers/ubuntu/nvm.sh
+++ preinstallers/ubuntu/nvm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir ~/.nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash -- -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
