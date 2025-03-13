8eaa7b3c6fecef1e098934b2630806c74c5f39d1
Chanzo Bryan <chanzob@netflix.com>
2025-03-13 10:05:09 -0700

    nvim: added octo for github

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -10,5 +10,18 @@ return {
       require('gitsigns').setup()
     end
   },
-}
 
+  {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      -- OR 'ibhagwan/fzf-lua',
+      -- OR 'folke/snacks.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require("octo").setup()
+    end
+  }
+}
