e21ce52703cd7a912ec54605a56d26f7eb51b2be
Chainso <chanzobryan@gmail.com>
2025-03-12 23:32:34 -0700

    nvim: creating attach configurations for java automatically through JavaReloadDap

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -1,11 +1,33 @@
 local dap = require("dap")
 
-dap.configurations.java = vim.tbl_extend('force', dap.configurations.java or {}, {
-  {
-    type = 'java',
-    request = 'attach',
-    name = "Debug (Attach) - Remote",
-    hostName = "127.0.0.1",
-    port = 5005,
-  }
-})
+local function extend_dap_configurations()
+  local new_configs = {}
+
+  for _, config in ipairs(dap.configurations.java or {}) do
+    table.insert(new_configs, config)
+    if config.request == 'launch' and config.projectName and config.mainClass then
+      table.insert(new_configs, vim.tbl_extend('force', {}, config, {
+        request = 'attach',
+        name = config.name .. " (Remote 5005)",
+        hostName = "127.0.0.1",
+        port = 5005,
+        projectName = config.projectName,
+        mainClass = config.mainClass,
+      }))
+    end
+  end
+
+  dap.configurations.java = new_configs
+end
+
+-- Function to extend DAP configurations
+local function reload_dap_configurations()
+  require("java").dap.config_dap()
+  extend_dap_configurations()
+end
+
+-- Create a user command to reload DAP configurations
+vim.api.nvim_create_user_command('JavaReloadDap', reload_dap_configurations, {})
+
+-- Initial extension of DAP configurations
+extend_dap_configurations()
