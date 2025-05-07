e1d056554698eaa4d2253eff7f7f9a34f114916d
Chanzo Bryan <chanzob@netflix.com>
2025-05-07 10:39:20 -0700

    nvim: added onedarkpro theme

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -1,8 +1,8 @@
-vim.cmd("colorscheme dracula")
+vim.cmd("colorscheme onedark")
 -- Calling twice fixed indent lines
-vim.cmd("colorscheme dracula")
+vim.cmd("colorscheme onedark")
 
-vim.cmd("hi DiffAdd guifg=#282a36 guibg=#50fa7b")
-vim.cmd("hi DiffChange guifg=#282a36 guibg=#50fa7b")
-vim.cmd("hi DiffText guifg=#ffffff guibg=#376c3b")
+-- vim.cmd("hi DiffAdd guifg=#282a36 guibg=#50fa7b")
+-- vim.cmd("hi DiffChange guifg=#282a36 guibg=#50fa7b")
+-- vim.cmd("hi DiffText guifg=#ffffff guibg=#376c3b")
 
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,5 +9,10 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000
+  },
+
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
   }
 }
