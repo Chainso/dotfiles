477332a040cafbbb41e65d742bb49075993c9822
Chanzo Bryan <chanzob@netflix.com>
2025-05-16 10:28:14 -0700

    nvim: mcphub unsetting port 3000

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -293,23 +293,7 @@ return {
     build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
     config = function()
       require("mcphub").setup({
-        -- Required options
-        port = 3000,                                                            -- Port for MCP Hub server
         config = vim.fn.expand(vim.fn.stdpath("config") .. "/mcpservers.json"), -- Absolute path to config filejava.
-
-        -- Optional options
-        on_ready = function(hub)
-          -- Called when hub is ready
-        end,
-        on_error = function(err)
-          -- Called on errors
-        end,
-        log = {
-          level = vim.log.levels.WARN,
-          to_file = false,
-          file_path = nil,
-          prefix = "MCPHub"
-        },
       })
     end
   },
