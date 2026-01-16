d6331487e1e6ab8b3a9d4212b0cd30cf76a1fbf2
Chainso <chanzobryan@gmail.com>
2026-01-16 15:58:12 -0800

    Using fdfind over fd if exists

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -4,7 +4,10 @@
 # Find Command
 # ------------
 # Setting fd as the default source for fzf
-export FZF_DEFAULT_COMMAND='fdfind -L -H --strip-cwd-prefix'
+## Check if 'fdfind' exists, otherwise default to 'fd'
+FD_CMD=$(command -v fdfind || command -v fd)
+
+export FZF_DEFAULT_COMMAND="$FD_CMD -L -H --strip-cwd-prefix"
 
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
